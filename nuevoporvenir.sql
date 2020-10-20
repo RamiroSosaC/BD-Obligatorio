6f67c1050c1c8850f651715d7e2541663b1db0b5
Ramiro <45610809+RamiroSosaC@users.noreply.github.com>
2020-10-19 21:34:04 -0300

    Update nuevoporvenir.sql

--- nuevoporvenir.sql
+++ nuevoporvenir.sql
@@ -133,5 +133,5 @@ SELECT* FROM clientes
 #LISTADO DE PROVEEDORES CON: RAZÓN SOCIAL, NOMBRE Y TELÉFONOS
 
 SELECT nombre_cont AS 'Nombre de Contacto', razsocial AS 'Razón Social', telproveedores.telefono AS 'Teléfono'
-FROM proveedores LEFT JOIN telproveedores ON proveedores.nrorut=telproveedores.nrorut; 	#GROUP BY nombre_cont;
+FROM proveedores LEFT JOIN telproveedores ON proveedores.nrorut=telproveedores.nrorut GROUP BY nombre_cont;
 
